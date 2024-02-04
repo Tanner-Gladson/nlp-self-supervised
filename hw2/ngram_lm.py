10ecd6114cbb28a17ba2d18eee0287770e2ae6a3
Tanner-Gladson <tannergladson1@gmail.com>
2024-02-04 12:47:54 -0500

    completed run_ngram function

--- hw2/ngram_lm.py
+++ hw2/ngram_lm.py
@@ -111,19 +111,35 @@ def generate_text(word_pred, tokenizer, n, prefix, max_len=100, stop_token="."):
     return tokenizer.convert_tokens_to_string(res)
 
 
+def largest_indices(arr, num_indices):
+    return np.argsort(arr)[len(arr) - num_indices:]
+
+
 # Plot ngram probabilities for the given context
 def plot_next_word_prob(word_scores, word_candidates, context, top=10, save_path=None):
-    # TODO: plot the top 10 next word probabilities after the given context
+    # TODONE: plot the top 10 next word probabilities after the given context
     # Hint: follow the hints in the pdf description for how to use matplotlib to plot the bar chart
     # - word_scores is a dictionary with tuple of the context as keys and an array of probabilities as values (the next_word_scores in create_ngrams function)
     # - word_candidates is a dictionary with tuple of the context as keys and a list of possible next words as values (the sorted_next_word_candidates in create_ngrams function)
     # - for a given context, elements in word_scores[context] and word_candidates[context] have one-to-one correspondence
     # - context is a tuple of words
     
+    scores = word_scores[context]
+    candidates = word_candidates[context]
 
-    raise NotImplementedError
-    # Your code ends here
+    indices_of_largest = largest_indices(scores, top)
+    top_scores = scores[indices_of_largest]
+    top_candidates = [candidates[i] for i in indices_of_largest]
 
+    # Plot the top candidates and their scores using matplotlib
+    plt.bar(top_candidates, top_scores)
+    plt.xlabel('Next Word')
+    plt.ylabel('Probability')
+    plt.title('Top 10 Next Word Probabilities')
+
+    # If the save_path is not None, save the plot as a pdf
+    if save_path is not None:
+        plt.savefig(save_path)
 
 def run_ngram():
     train_data, dev_data = load_data()
@@ -132,7 +148,7 @@ def run_ngram():
     sentence_split_and_tokenize_demo(train_data, splitter, tokenizer)
     word_pred, next_word_scores, next_word_candidates = create_ngrams(train_data[:100000], 3, splitter, tokenizer)
 
-    # TODO: paste the generated plots to the pdf
+    # TODONE: paste the generated plots to the pdf (done in the plot function)
     context1 = ("move", "to")
     context2 = ("the", "news")
     print(f"{'-' * 10} plot the top 10 next word probabilities after {context1} {'-' * 10}")
@@ -149,3 +165,11 @@ def run_ngram():
     print(completion1)
     print(f"{'-' * 10} generated text 2 {'-' * 10}")
     print(completion2)
+
+    # We want to save the completions to a PDF file, not just plain text
+    with open("ngram_completions1.txt", "w") as f:
+        f.write(completion1)
+    
+    with open("ngram_completions2.txt", "w") as f:
+        f.write(completion2)
+
