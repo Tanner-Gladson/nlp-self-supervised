84591ea25b84563778e8dab3b6b03a006d97518b
Tanner Gladson <tannergladson@gmail.com>
2024-02-27 14:41:50 -0500

    another typo

--- hw5/gpt/model.py
+++ hw5/gpt/model.py
@@ -73,7 +73,7 @@ class CausalSelfAttention(nn.Module):
         # causal self-attention; Self-attend: (`B`, n_head, T, n_embd / n_head) x (B, n_head, n_embd / n_head, T) -> (B, n_head, T, T)
         # calculate the scaled dot-product attention with causal mask, name the attention matrix as `att`
         # step 1: q @ k^T / sqrt(d_k), where d_k is the head hidden dimension (n_embd / n_head)
-        self_attend = q @ k.transpose(-2, -1) / math.sqrt(C // self.n_head)
+        att = q @ k.transpose(-2, -1) / math.sqrt(C // self.n_head)
 
         # step 2: apply the causal mask to the attention matrix
         # the masked out entries in att should have the value of float('-inf')
@@ -82,19 +82,19 @@ class CausalSelfAttention(nn.Module):
         # - don't forget to truncate the mask to the actual sequence length (T) # For each sequence, we resize the mask instead of padding!
         # - to apply the mask, one possible way is the `torch.Tensor.masked_fill` method
         # - - for this, you can obtain the boolean mask by element-wise comparison of the causal mask with 0
-        self_attened = self_attend.masked_fill(self.causal_mask[:, :, :T, :T] == 0, float('-inf'))
+        att = att.masked_fill(self.causal_mask[:, :, :T, :T] == 0, float('-inf'))
 
         # step 3: apply the softmax function to the masked attention matrix
         # hint: you can use the `F.softmax` function
-        self_attened = F.softmax(self_attend, dim=3) # Last dimension, maybe use -1 instead?
+        att = F.softmax(att, dim=3) # Last dimension, maybe use -1 instead?
 
         # step 4: apply the attention dropout (self.attn_dropout) to the attention matrix
-        self_attened = self.attn_dropout(self_attened)
+        att = self.attn_dropout(att)
 
         # step 5: multiply the attention matrix with value vectors
         # I think this will be like (B, n_head, T, T) x (B, n_head, T, n_embd / n_head) -> (B, n_head, T, n_embd / n_head)
         # Why is the n_embd the size outputted by all the heads? Doesn't seem the most intuitive
-        y = self_attened @ v 
+        y = att @ v 
 
         # your code ends here
 
